dda36c0fc330ac6622c0223a692eba50457e9d5e
georgee <jurajbosy1@gmail.com>
2023-02-19 22:19:16 +0100

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsTwitter } from "react-icons/bs";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for NewsLetter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group py-1">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Your Email Address"
+                  aria-label="Your Email Address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div className="footer-links d-flex flex-column">
+                <address className="text-white fs-6">
+                  Hno : 277 Near Vill chopal,
+                  <br /> Sonipat, Haryna <br />
+                  PinCode: 131103
+                </address>
+                <a
+                  href="tel: +91 8264954234"
+                  className="mt-2 d-block mb-1 text-white"
+                >
+                  91 826 495 4234
+                </a>
+                <a
+                  href="mailto:navdeepgahiya781@gmail.com"
+                  className="mt-2 d-block mb-0 text-white"
+                >
+                  navdeepgahiya781@gmail.com
+                </a>
+                <div className="social-icons d-flex align-items-center gap-15 mt-3">
+                  <a href="" alt="social icon">
+                    <BsLinkedin className="text-white fs-2" />
+                  </a>
+                  <a href="" alt="social icon">
+                    <BsGithub className="text-white fs-2" />
+                  </a>
+                  <a href="" alt="social icon">
+                    <BsTwitter className="text-white fs-2" />
+                  </a>
+                  <a href="" alt="social icon">
+                    <BsYoutube className="text-white fs-2" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms of Service</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Search</Link>
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">Faq</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}: Powered by Developers Corner
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner  position-relative">
+                <img
+                  src="images/main-banner-1.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.00 or 41.62/mo.</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>BEST SALE</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      From $999.00 <br /> 41.62/mo.
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>But IPad Air</h5>
+                    <p>
+                      From $599.00 <br /> 12/mo.
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-03.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>15% OFF</h4>
+                    <h5>Smartwatch 7</h5>
+                    <p>
+                      Shop the latest band <br /> styles and colors.
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner  position-relative">
+                  <img
+                    src="images/catbanner-04.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>FREE ENGRAVING</h4>
+                    <h5>AirPods Max</h5>
+                    <p>
+                      High-fidelity playback & <br /> ultra-low distortion
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
